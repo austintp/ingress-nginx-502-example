abe67b725cde64f383a439f5487bab9d670666af
Austin Peters <austin.peters@kineticdata.com>
2020-02-07 12:44:28 -0600

    Removing SSL and sidecars to simplify example

--- platform-services/subdomain-provisioner/files/scripts/provision-subdomain.sh
+++ platform-services/subdomain-provisioner/files/scripts/provision-subdomain.sh
@@ -3,6 +3,4 @@
 helm template \
   --set host=$K8S_HOSTNAME \
   --set-string tenant=$1 \
-  --set-string sidecar.tls_crt="$(cat /app/cert/tls.crt | base64 | tr -d '\n')" \
-  --set-string sidecar.tls_key="$(cat /app/cert/tls.key | base64 | tr -d '\n')" \
   subdomain-templates | kubectl apply -f -
